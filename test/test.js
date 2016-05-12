aa527466d0df1037d848113c63f1a65f5dd250cb
Permutator <permutatorem@gmail.com>
2016-05-11 17:52:57 -0700

    Added some more tests

--- test/test.js
+++ test/test.js
@@ -41,3 +41,31 @@ it("should let nothing through if no options are passed", function() {
     plugins: [hypothetical()]
   }), "does not exist in the hypothetical file system");
 });
+
+it("should be able to simulate an entry file", function() {
+  return resolve(rollup.rollup({
+    entry: './x.js',
+    plugins: [hypothetical({ files: { './x.js': 'object.key = false;' } })]
+  }), { key: false });
+});
+
+it("should be able to simulate an entry file where one already exists", function() {
+  return resolve(rollup.rollup({
+    entry: './test/fixtures/a.js',
+    plugins: [hypothetical({ files: { './test/fixtures/a.js': 'object.key = false;' } })]
+  }), { key: false });
+});
+
+it("should handle an entry point that appears to be external", function() {
+  return resolve(rollup.rollup({
+    entry: 'x.js',
+    plugins: [hypothetical({ files: { './x.js': 'object.key = false;' } })]
+  }), { key: false });
+});
+
+it("should handle absolute paths", function() {
+  return resolve(rollup.rollup({
+    entry: '/x.js',
+    plugins: [hypothetical({ files: { '/x.js': 'object.key = false;' } })]
+  }), { key: false });
+});
